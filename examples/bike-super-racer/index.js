23ed1e4d56e08b4520dc65274d918673fbb0bff0
Rastislav Lamos <lamos.rasto@gmail.com>
2016-01-29 10:51:05 +0100

    Update example with comments, stop after 10 secs

--- examples/bike-super-racer/index.js
+++ examples/bike-super-racer/index.js
@@ -1,12 +1,29 @@
 import RpmMeter, { PULSE_EVENT } from "spin-bike-rpm-meter";
 
-let rpmMeter = new RpmMeter();
+const rpmMeter = new RpmMeter();
 
+// Start listening to the "pulses". Asks for permission to the microphone. Run start() before
+// starting to cycle on the spin bike.
 rpmMeter.start().then((stop) => {
-	console.log(stop);
+	console.log('Starting listening to pulses...');
 
-	rpmMeter.on(PULSE_EVENT, (params) => {
-		console.log(params);
+	// After each revolution on the bike, a PULSE_EVENT is emitted containing some useful data about it.
+	rpmMeter.on(PULSE_EVENT, (pulseData) => {
+		console.log(pulseData);
+
+		// This is how the pulseData might look
+		//pulseData = {
+		//	'timestamp': 1454060831,
+		//	'secondsBetweenPulses': 0.8,
+		//	'rpm': 75
+		//};
 
 	});
+
+	// Stop listening to the "pulses" after 10 seconds.
+	setTimeout(() => {
+		stop();
+		
+		console.log('Stopped listening to pulses...');
+	}, 10000);
 });
